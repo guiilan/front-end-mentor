2fc090a39a24131e5a08b79c05f0dbb0e977dcf5
guiilan <gui.tioze@gmail.com>
2021-03-31 22:46:32 -0300

    animação do menu

--- js/index.js
+++ js/index.js
@@ -7,22 +7,26 @@ btnMenu.addEventListener('click', function(){
     var boxMenu = document.querySelector('.fm-menu-mobile');
 
     boxMenu.classList.toggle('fm-is-open')
+    boxMenu.classList.toggle('fm-slideMenu')
 })
 
 btnProduct.addEventListener('click', function () {
     var boxSubMenuProduct = document.querySelector('#ProductSub');
 
     boxSubMenuProduct.classList.toggle('fm-is-open')
+    boxSubMenuProduct.classList.toggle('fm-slideSubMenu')
 })
 
 btnCompany.addEventListener('click', function() {
     var boxSubMenuCompany = document.querySelector('#CompanySub');
 
     boxSubMenuCompany.classList.toggle('fm-is-open')
+    boxSubMenuCompany.classList.toggle('fm-slideSubMenu')
 })
 
 btnConnect.addEventListener('click', function() {
     var boxSubMenuConnect = document.querySelector('#ConnectSub');
 
     boxSubMenuConnect.classList.toggle('fm-is-open')
+    boxSubMenuConnect.classList.toggle('fm-slideSubMenu')
 })
